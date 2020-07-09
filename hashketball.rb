c1144f95d2478843eb190c902e67e05718252f91
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-09 22:12:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
   #The top level of the hash has two keys: :home, for the home team, and :away, for the away team
   :home{
   
-:team_name =>
+team_name:
 :colors =>
 :players =>
   }
